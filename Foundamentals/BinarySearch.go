c11b990c71ba1815269b693011cee5db58142bdb
z-wentao <1728679818@qq.com>
2025-05-04 10:42:30 +0800

    binary search

---
+++ Foundamentals/BinarySearch.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+var o = []int{-5, -3, 1, 4, 7}
+
+func binary_search(r int, l int, s int) int {
+	if r == l {
+		return -1
+	}
+	mid := (r + l) >> 1
+	if o[mid] == s {
+		return mid
+	} else if o[mid] > s {
+		return binary_search(r, mid, s)
+	} else {
+		return binary_search(mid+1, l, s)
+	}
+}
+
+func main() {
+	a := binary_search(0, len(o), -3)
+	if a == 1 {
+		fmt.Println("ok")
+	}
+	if binary_search(0, len(o), -4) == -1 {
+		fmt.Println("ok")
+	}
+	if binary_search(0, len(o), -5) == 0 {
+		fmt.Println("ok")
+	}
+	if binary_search(0, len(o), 9) == -1 {
+		fmt.Println("ok")
+	}
+}
