b4a4159c467aa83e85d360b7414e0edf43a89675
z-wentao <1728679818@qq.com>
2025-05-19 22:00:20 +0800

    SearchInsertPosition

---
+++ LeetCode_150/SearchInsertPosition.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func searchInsert(nums []int, target int) int {
+	left, right := 0, len(nums)
+	for left < right {
+		mid := (left + right) >> 1
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid
+		}
+	}
+	return left
+}
+
+func main() {
+	var nums = []int{1, 3, 5, 6}
+	fmt.Println(searchInsert(nums, 5))
+	fmt.Println(searchInsert(nums, 2))
+	fmt.Println(searchInsert(nums, 7))
+	fmt.Println(searchInsert(nums, 4))
+}
